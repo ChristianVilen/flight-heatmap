91b1e59202311dad6209c3736accb0c8835625e0
Christian Vilen <c.vilen@outlook.com>
2025-07-25 16:38:11 +0300

    Adjust map settings, poll data

--- web/src/lib/components/Map.svelte
+++ web/src/lib/components/Map.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
-  import L, { bounds, Map as LeafletMap } from "leaflet";
+  import { onMount, onDestroy } from "svelte";
+  import L, { Map as LeafletMap, type HeatLayer } from "leaflet";
   import "leaflet/dist/leaflet.css";
   import "leaflet.heat";
 
   type HeatPoint = {
-    lat_bin: number;
-    lon_bin: number;
-    count: number;
+    LatBin: number;
+    LonBin: number;
+    Count: number; // intensity
   };
 
+  const POLL_INTERVAL_10_SEC = 10000;
+
   let map: LeafletMap | null = null;
+  let heatLayer: HeatLayer;
 
   const helsinkiAirportCoords: [number, number] = [60.3172, 24.9633];
 
@@ -32,31 +36,39 @@
     return m;
   }
 
-  async function fetchAndRenderHeat() {
+  async function fetchHeatData(): Promise<[number, number, number][]> {
     const res = await fetch("/api/heatmap");
     const points: HeatPoint[] = await res.json();
-    const heatData = points.map((p) => [p.LatBin, p.LonBin, p.Count]);
 
-    L.heatLayer(heatData, {
+    return points.map((p) => [p.LatBin, p.LonBin, p.Count]);
+  }
+
+  async function updateHeatmap() {
+    const heatData = await fetchHeatData();
+    if (heatLayer) {
+      heatLayer.setLatLngs(heatData);
+    }
+  }
+
+  let interval: ReturnType<typeof setInterval>;
+
+  onMount(async () => {
+    map = createMap(document.getElementById("map")!);
+
+    const initialData = await fetchHeatData();
+    heatLayer = L.heatLayer(initialData, {
       radius: 15,
       blur: 10,
       maxZoom: 14,
-      bounds: map?.getBounds(),
     }).addTo(map);
-  }
 
-  function mapAction(container: HTMLElement) {
-    map = createMap(container);
+    interval = setInterval(updateHeatmap, POLL_INTERVAL_10_SEC);
+  });
 
-    fetchAndRenderHeat();
-
-    return {
-      destroy: () => {
-        map?.remove();
-        map = null;
-      },
-    };
-  }
+  onDestroy(() => {
+    clearInterval(interval);
+    map?.remove();
+  });
 
   function resizeMap() {
     map?.invalidateSize();
@@ -65,7 +77,4 @@
 
 <svelte:window on:resize={resizeMap} />
 
-<div use:mapAction class="w-full h-full"></div>
-
-<style>
-</style>
+<div id="map" class="w-full h-full"></div>
